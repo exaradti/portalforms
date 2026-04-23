9f58adc93c6cc48f161f0fb0aff24530127d1341
exaradti <exaradti@gmail.com>
2026-04-23 16:01:33 -0300

    Delete assets directory

--- assets/supabase-config.js
+++
@@ -1,4 +0,0 @@
-window.SUPABASE_CONFIG = {
-  url: "https://pnavgbkadmbrapkeqrjr.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBuYXZnYmthZG1icmFwa2VxcmpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMjczMDAsImV4cCI6MjA5MTgwMzMwMH0.e-idoGkYQzywQNrr-H6R7vKAqZluC5vquCFi90eSWow"
-};
